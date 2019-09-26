5843ef9c405f8c995c7e3eaf2e7112312cfab60c
Suraj Khairnar <39847@hexaware.com>
2019-09-26 22:54:07 +0530

    Commit from User Y

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -7,7 +7,9 @@ public class RESTAPITest {
 	@Test
 	public void postJira()
 	{
-		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
 	}
 	
 	@Test
